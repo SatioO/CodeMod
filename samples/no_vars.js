76cf15d8f84432c2211f13de65c2f76d57bfc7e1
Brain Vaibhav Satam <vaibhav.satam29991@gmail.com>
2019-09-27 18:04:14 +0530

    fix(code)

--- samples/no_vars.js
+++ samples/no_vars.js
@@ -4,15 +4,18 @@
  * replaced with const
  */
 module.exports = `
+import { one } from './a';
+import { two } from './a';
+import { three } from './a';
 
-    import { one } from './a';
-    import { two } from './a';
-    import { three } from './a';
+var a = 10;
+a = 20;
 
-    var a = 10;
-    a = 20;
+var b = 40;
 
-    var b = 40;
+console.log(b);
 
-    console.log(b);
+module.exports.one = one;
+module.exports.two = two;
+exports.three = three;
 `
