194fb7a6151c900e58fab981b8c9d536b22bedcb
Brain Vaibhav Satam <vaibhav.satam29991@gmail.com>
2019-09-27 18:07:23 +0530

    fix(code)

--- samples/clean_import.js
+++ samples/clean_import.js
@@ -1,6 +1,16 @@
 module.exports = `
-    import { one } from './a';
-    import { two } from './a';
-    import { three } from './a';
-    import _ from "lodash"
-`;
+import { one } from './a';
+import { two } from './a';
+import { three } from './a';
+
+var a = 10;
+a = 20;
+
+var b = 40;
+
+console.log(b);
+
+module.exports.one = one;
+module.exports.two = two;
+exports.three = three;
+`
