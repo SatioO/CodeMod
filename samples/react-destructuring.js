f89600422f1d622a264908dcef88b8007950b3f4
Brain Vaibhav Satam <vaibhav.satam29991@gmail.com>
2019-09-27 17:48:58 +0530

    react

--- samples/react-destructuring.js
+++ samples/react-destructuring.js
@@ -6,8 +6,16 @@ module.exports = `
 import React from 'react';
 
 class C extends React.Component() {
+    constructor() {
+        this.onClick = this.onClick.bind(this);
+        this.onMove = this.onMove.bind(this);
+    }
+
+    onClick() {}
+    onMove() {}
+    
     render() {
         return <div foo={this.props.foo} bar={this.props.bar} />;
     }
 }
-`;
+`
